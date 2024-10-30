7ebf7f59b275df66a8714d165e84dc69e7ad8f3d
Willyan Costa <willyancr@gmail.com>
2024-10-29 21:24:23 -0300

    update route.ts

--- src/app/api/get-watchlist/route.ts
+++ src/app/api/get-watchlist/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/app/lib/prisma";
+import { authOptions } from "@/auth.config";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
   try {
