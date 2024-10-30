be778265f24155ddacbb3efdfe4cc0b642c64f77
Willyan Costa <willyancr@gmail.com>
2024-10-29 21:23:06 -0300

    chore: Refactor authentication setup to use centralized config
    Extracted authentication options to a centralized config file for better
    organization and reusability across different routes. This simplifies
    maintenance and ensures consistency in authentication settings.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/app/lib/prisma";
+import { authOptions } from "@/auth.config";
+import NextAuth from "next-auth";
 
-// Configuração do NextAuth
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id; // Adiciona o ID do usuário na sessão
-      }
-      return session;
-    },
-  },
-};
-
-// Exporta o NextAuth diretamente
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ src/auth.config.ts
@@ -0,0 +1,22 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import { AuthOptions } from "next-auth";
+import { prisma } from "./app/lib/prisma";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+};
